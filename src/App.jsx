10b7941d8a88b12985143dbcae999d583e125fff
Alex Pop <Alex.Pop@VERIFONE.com>
2023-07-27 11:54:22 +0300

    jsx basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+
 function App() {
-  return "Hello World";
+  // return <label>{2 + 2}</label>;
+  // return <input type="text"></input>;
+  // const myCustomLabel = <label htmlFor="inputId">anyText</label>;
+  // return (
+  //   <div id="largeDiv" className="large">
+  //     {myCustomLabel}
+  //     <input id="inputId" type="number" defaultValue={3}></input>
+  //   </div>
+  // );
 }
 
 export default App;
+
+<div></div>;
