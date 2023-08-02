16806a0868225ff3b84fa09edfd53f1a431d3626
alexandrupp <alexandrup@ensemble.com>
2023-08-02 14:32:43 +0300

    31 | strict mode

---
+++ 31-strict-mode/src/Child.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export function Child() {
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    console.log("Mount");
+
+    return () => console.log("Unmount");
+  }, []);
+
+  useEffect(() => {
+    console.log(`My name is ${name} and I am ${age} years old.`);
+  }, [name, age]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={() => setAge((a) => a - 1)}>-</button>
+      {age}
+      <button onClick={() => setAge((a) => a + 1)}>+</button>
+      <br />
+      <br />
+      My name is {name} and I am {age} years old.
+    </div>
+  );
+}
