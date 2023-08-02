f041d5be02c3f96ff4703548a80e9ddc2a9a23f2
alexandrupp <alexandrup@ensemble.com>
2023-08-02 10:58:57 +0300

    25 | use effect hook part 2

--- 25-use-effect-hook/src/Child.jsx
+++ 25-use-effect-hook/src/Child.jsx
@@ -3,10 +3,33 @@ import { useState, useEffect } from "react";
 export function Child() {
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     console.log("Name changed");
   }, [name]);
+
+  useEffect(() => {
+    document.title = `My age is ${age}`;
+  }, [age]);
+
+  useEffect(() => {
+    console.log("Mounted component");
+  }, []);
+
+  useEffect(() => {
+    // everything runs after cleanup function
+    const handler = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handler);
+
+    // cleanup function
+    return () => {
+      window.removeEventListener("resize", handler);
+    };
+  }, []);
 
   return (
     <div>
@@ -17,6 +40,7 @@ export function Child() {
       <p>
         My name is {name} and I am {age} years old
       </p>
+      <p>My screen width is {width}</p>
     </div>
   );
 }
