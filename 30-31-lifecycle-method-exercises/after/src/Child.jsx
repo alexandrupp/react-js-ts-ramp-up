031084e6f0d34afcaec98640c507ea562d7c104f
alexandrupp <alexandrup@ensemble.com>
2023-08-02 13:50:18 +0300

    30 | class component lifecycle methods

--- 30-31-lifecycle-method-exercises/after/src/Child.jsx
+++ 30-31-lifecycle-method-exercises/after/src/Child.jsx
@@ -1,12 +1,47 @@
-import React from "react"
+import React from "react";
 
 export class Child extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
     this.state = {
       age: 0,
       name: "",
+    };
+  }
+
+  componentDidMount() {
+    console.log("Hi");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Re-Render");
+    if (
+      prevState.name !== this.state.name ||
+      prevState.age !== this.state.age
+    ) {
+      console.log(
+        `My name is ${this.state.name} and I am ${this.state.age} years old`
+      );
     }
+    if (prevState.name !== this.state.name) {
+      document.title = this.state.name;
+
+      if (this.timeout !== null) {
+        clearTimeout(this.timeout);
+      }
+
+      this.timeout = setTimeout(() => {
+        console.log(`My name is ${this.state.name}`);
+      }, 1000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timeout !== null) {
+      clearTimeout(this.timeout);
+    }
+
+    console.log("Bye");
   }
 
   render() {
@@ -15,14 +50,14 @@ export class Child extends React.Component {
         <input
           type="text"
           value={this.state.name}
-          onChange={e => this.setState({ name: e.target.value })}
+          onChange={(e) => this.setState({ name: e.target.value })}
         />
         <br />
         <br />
         <button
           onClick={() =>
-            this.setState(state => {
-              return { age: state.age - 1 }
+            this.setState((state) => {
+              return { age: state.age - 1 };
             })
           }
         >
@@ -31,8 +66,8 @@ export class Child extends React.Component {
         {this.state.age}
         <button
           onClick={() =>
-            this.setState(state => {
-              return { age: state.age + 1 }
+            this.setState((state) => {
+              return { age: state.age + 1 };
             })
           }
         >
@@ -42,6 +77,6 @@ export class Child extends React.Component {
         <br />
         My name is {this.state.name} and I am {this.state.age} years old.
       </div>
-    )
+    );
   }
 }
