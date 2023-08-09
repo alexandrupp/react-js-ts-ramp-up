b0f0d606fd5e6b4aed707959a48a24e8e3ad7e65
alexandrupp <alexandrup@ensemble.com>
2023-08-09 15:37:54 +0300

    52 | useArray hook

--- 51-52-use-array-hook/after/src/useArray.js
+++ 51-52-use-array-hook/after/src/useArray.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 export function useArray(initialValue) {
   const [array, setArray] = useState(initialValue);
 
-  const push = () => {};
-  const replace = () => {};
-  const filter = () => {};
-  const remove = () => {};
-  const clear = () => {};
-  const reset = () => {};
+  const push = (element) => setArray([...array, element]);
+  const replace = (index, newElem) =>
+    setArray(array.map((elem, id) => (id === index ? newElem : elem)));
+  const filter = (condition) => setArray(array.filter(condition));
+  const remove = (elemAtIndex) => filter((n, id) => id !== elemAtIndex);
+  const clear = () => setArray([]);
+  const reset = () => setArray(initialValue);
 
   return { array, set: setArray, push, replace, filter, remove, clear, reset };
 }
