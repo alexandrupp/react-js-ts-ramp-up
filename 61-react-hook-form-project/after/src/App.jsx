9952376cb9defaca9b7d8e1a2d6ba5bbf9c55118
alexandrupp <alexandrup@ensemble.com>
2023-08-15 15:02:39 +0300

    61 | react hook form implementation

--- 61-react-hook-form-project/after/src/App.jsx
+++ 61-react-hook-form-project/after/src/App.jsx
@@ -1,53 +1,58 @@
-import { FormGroup } from "./FormGroup"
-import ReactSelect from "react-select"
-import { useRef, useState } from "react"
-import "./styles.css"
-import { checkCountry, checkEmail, checkPassword } from "./validators"
+import { FormGroup } from "./FormGroup";
+import ReactSelect from "react-select";
+import "./styles.css";
+import { useController, useForm } from "react-hook-form";
 
 const COUNTRY_OPTIONS = [
   { label: "United States", value: "US" },
   { label: "India", value: "IN" },
   { label: "Mexico", value: "MX" },
-]
+];
 
 function App() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const countryRef = useRef()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    watch,
+  } = useForm();
 
-  const [emailErrors, setEmailErrors] = useState([])
-  const [passwordErrors, setPasswordErrors] = useState([])
-  const [countryErrors, setCountryErrors] = useState([])
+  const { field: countryField } = useController({
+    name: "country",
+    control,
+    rules: { required: { value: true, message: "Required" } },
+  });
 
-  function onSubmit(e) {
-    e.preventDefault()
+  const email = watch("email");
+  console.log(email);
 
-    const emailResults = checkEmail(emailRef.current.value)
-    const passwordResults = checkPassword(passwordRef.current.value)
-    const countryResults = checkCountry(countryRef.current.getValue()[0])
-
-    setEmailErrors(emailResults)
-    setPasswordErrors(passwordResults)
-    setCountryErrors(countryResults)
-
-    if (
-      emailResults.length === 0 &&
-      passwordResults.length === 0 &&
-      countryResults.length === 0
-    ) {
-      alert("Success")
-    }
+  function onSubmit(data) {
+    console.log(data);
+    alert("Success");
   }
 
   return (
-    <form onSubmit={onSubmit} className="form">
-      <FormGroup errors={emailErrors}>
+    <form onSubmit={handleSubmit(onSubmit)} className="form">
+      <FormGroup errorMessage={errors?.email?.message}>
         <label className="label" htmlFor="email">
           Email
         </label>
-        <input className="input" type="email" id="email" ref={emailRef} />
+        <input
+          className="input"
+          type="email"
+          id="email"
+          {...register("email", {
+            required: { value: true, message: "Required" },
+            validate: (value) => {
+              if (!value.endsWith("@webdevsimplified.com")) {
+                return "Must end with @webdevsimplified.com";
+              }
+            },
+          })}
+        />
       </FormGroup>
-      <FormGroup errors={passwordErrors}>
+      <FormGroup errorMessage={errors?.password?.message}>
         <label className="label" htmlFor="password">
           Password
         </label>
@@ -55,10 +60,30 @@ function App() {
           className="input"
           type="password"
           id="password"
-          ref={passwordRef}
+          {...register("password", {
+            required: { value: true, message: "Required" },
+            minLength: { value: 10, message: "Must be at least 10 characters" },
+            validate: {
+              hasLowerCase: (value) => {
+                if (!value.match(/[a-z]/)) {
+                  return "Must include at least 1 lowercase letter";
+                }
+              },
+              hasUpperCase: (value) => {
+                if (!value.match(/[A-Z]/)) {
+                  return "Must include at least 1 uppercase letter";
+                }
+              },
+              hasNumber: (value) => {
+                if (!value.match(/[0-9]/)) {
+                  return "Must include at least 1 number";
+                }
+              },
+            },
+          })}
         />
       </FormGroup>
-      <FormGroup errors={countryErrors}>
+      <FormGroup errorMessage={errors?.country?.message}>
         <label className="label" htmlFor="country">
           Country
         </label>
@@ -66,15 +91,15 @@ function App() {
           isClearable
           classNamePrefix="react-select"
           id="country"
-          ref={countryRef}
           options={COUNTRY_OPTIONS}
+          {...countryField}
         />
       </FormGroup>
       <button className="btn" type="submit">
         Submit
       </button>
     </form>
-  )
+  );
 }
 
-export default App
+export default App;
