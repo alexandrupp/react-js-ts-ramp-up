12078d4b3b53ee7bfad474af72745e65a67cede6
alexandrupp <alexandrup@ensemble.com>
2023-08-15 16:51:42 +0300

    62 | use reducer hook

--- 62-use-reducer-hook/src/App.jsx
+++ 62-use-reducer-hook/src/App.jsx
@@ -1,5 +1,70 @@
 import { Counter } from "./Counter";
+import { useState } from "react";
+import { useFetch } from "./useFetch";
 
-export default function App() {
-  return <Counter />;
+const URLS = {
+  USERS: "https://jsonplaceholder.typicode.com/users",
+  POSTS: "https://jsonplaceholder.typicode.com/posts",
+  COMMENTS: "https://jsonplaceholder.typicode.com/comments",
+};
+
+// BONUS:
+// const OPTIONS = {
+//   method: "POST",
+//   body: JSON.stringify({ name: "Kyle" }),
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+// };
+
+function App() {
+  const [url, setUrl] = useState(URLS.USERS);
+
+  const { data, isError, isLoading } = useFetch(url);
+  // BONUS:
+  // const { data, isLoading, isError } = useFetch(url, OPTIONS);
+
+  return (
+    <>
+      <div>
+        <Counter />
+        <hr />
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            checked={url === URLS.USERS}
+            onChange={() => setUrl(URLS.USERS)}
+          />
+          Users
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={url === URLS.POSTS}
+            onChange={() => setUrl(URLS.POSTS)}
+          />
+          Posts
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={url === URLS.COMMENTS}
+            onChange={() => setUrl(URLS.COMMENTS)}
+          />
+          Comments
+        </label>
+      </div>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : isError ? (
+        <h1>Error</h1>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
+    </>
+  );
 }
+
+export default App;
--- 62-use-reducer-hook/src/Counter.jsx
+++ 62-use-reducer-hook/src/Counter.jsx
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const ACTIONS = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT",
+  RESET: "RESET",
+  PLUS_FIVE: "PLUS_FIVE",
+};
+
+function reducer(count, action) {
+  switch (action.type) {
+    case ACTIONS.DECREMENT:
+      return count - 1;
+    case ACTIONS.INCREMENT:
+      return count + 1;
+    case ACTIONS.RESET:
+      return 0;
+    case ACTIONS.PLUS_FIVE:
+      return count + action.payload.value;
+    default:
+      return count;
+  }
+}
 
 export function Counter({ initialCount = 0 }) {
-  const [count, setCount] = useState(initialCount);
+  const [count, dispatch] = useReducer(reducer, initialCount);
 
   return (
     <>
-      <button onClick={() => setCount((c) => c - 1)}>-</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DECREMENT })}>-</button>
       {count}
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <button onClick={() => dispatch({ type: ACTIONS.INCREMENT })}>+</button>
+      <button onClick={() => dispatch({ type: ACTIONS.RESET })}>Reset</button>
+      <button
+        onClick={() =>
+          dispatch({ type: ACTIONS.PLUS_FIVE, payload: { value: 5 } })
+        }
+      >
+        +5
+      </button>
     </>
   );
 }
